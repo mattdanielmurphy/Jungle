6f228c12ea2fe6812029e43c81ea15174746bd36
Matthew Daniel Murphy <matt@mattmurphy.ca>
2018-01-19 13:43:46 -0500

    change symbol arrays to use %i format; rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: %i[index show]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
@@ -10,12 +10,12 @@ Rails.application.routes.draw do
     delete :remove_item
   end
 
-  resources :orders, only: [:create, :show]
+  resources :orders, only: %i[create show]
   resources :orders, only: %i[create show]
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :products, except: %i[edit update show]
     resources :products, except: %i[edit update show]
 
     resources :categories
