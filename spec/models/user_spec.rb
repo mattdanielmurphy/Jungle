3a45cca04c9e7a9a896853efde850dfc61a6bb74
Matthew Daniel Murphy <matt@mattmurphy.ca>
2018-01-22 20:38:01 -0500

    Add testing for user authentication class

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 describe User, type: :model do
-  describe 'Validations:' do
-    before(:each) do
-      @user = User.create(
-        firstname: 'Famous',
-        lastname: 'Mortimer',
-        email: 'famous@mail.com',
-        password: 'strong_password',
-        password_confirmation: 'strong_password'
-      )
-    end
+  before(:each) do
+    @user = User.create(
+      firstname: 'Famous',
+      lastname: 'Mortimer',
+      email: 'famous@mail.com',
+      password: 'strong_password',
+      password_confirmation: 'strong_password'
+    )
+  end
 
+  describe 'Validations:' do
     it 'should return valid' do
       expect(@user.valid?).to eq(true)
     end
@@ -32,6 +32,26 @@ describe User, type: :model do
     it 'should return invalid if email not unique' do
       @user2 = @user.dup
       expect(@user2.valid?).to eq(false)
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it 'should return valid if credentials correct' do
+      expect(
+        User.authenticate_with_credentials('famous@mail.com', 'strong_password')
+      ).to_not be_nil
+    end
+
+    it 'should return invalid if password incorrect' do
+      expect(
+        User.authenticate_with_credentials('famous@mail.com', 'wrong_password')
+      ).to be_nil
+    end
+
+    it 'should return invalid if email incorrect' do
+      expect(
+        User.authenticate_with_credentials('wrong@email.com', 'strong_password')
+      ).to be_nil
     end
   end
 end
