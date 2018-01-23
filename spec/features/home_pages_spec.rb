061ef32e8bfec917b3b390d5c84f10e136158ec6
Matthew Daniel Murphy <matt@mattmurphy.ca>
2018-01-23 16:34:03 -0500

    add testing for products on index

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature 'Visitor navigates to home page', type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
   scenario 'user sees all products' do
     visit root_path
 
     save_screenshot
+
     expect(page).to have_css 'article.product'
   end
 end
