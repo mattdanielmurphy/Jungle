7a386a5d1b7f99cae4c327b64797d7f3f87f41c0
Matthew Daniel Murphy <matt@mattmurphy.ca>
2018-01-22 21:28:16 -0500

    Rewrite test descriptions for coherence & create function for brevity
    function for authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,45 +12,49 @@ describe User, type: :model do
   end
 
   describe 'Validations:' do
-    it 'should return valid' do
+    it 'should validate' do
       expect(@user.valid?).to eq(true)
     end
 
     context 'Password:' do
-      it 'should return invalid if password & confirmation don\'t match' do
+      it 'should not validate if password & confirmation don\'t match' do
         @user.password              = 'xxxxxxxxxxx'
         @user.password_confirmation = 'zzzzzzzzzz'
         expect(@user.valid?).to eq(false)
       end
 
-      it 'should return invalid if password less than 10 characters long' do
+      it 'should not validate if password less than 10 characters long' do
         @user.password = @user.password_confirmation = 'shortpass'
         expect(@user.valid?).to eq(false)
       end
     end
 
-    it 'should return invalid if email not unique' do
+    it 'should not validate if email non-unique' do
       @user2 = @user.dup
       expect(@user2.valid?).to eq(false)
     end
   end
 
-  describe '.authenticate_with_credentials' do
-    it 'should return valid if credentials correct' do
+  describe '.authenticate_with_credentials:' do
+    def authenticate(email, password)
+      User.authenticate_with_credentials(email, password)
+    end
+
+    it 'should authenticate if credentials correct' do
       expect(
-        User.authenticate_with_credentials('famous@mail.com', 'strong_password')
+        authenticate('famous@mail.com', 'strong_password')
       ).to_not be_nil
     end
 
-    it 'should return invalid if password incorrect' do
+    it 'should not authenticate if password incorrect' do
       expect(
-        User.authenticate_with_credentials('famous@mail.com', 'wrong_password')
+        authenticate('famous@mail.com', 'wrong_password')
       ).to be_nil
     end
 
-    it 'should return invalid if email incorrect' do
+    it 'should not authenticate if email incorrect' do
       expect(
-        User.authenticate_with_credentials('wrong@email.com', 'strong_password')
+        authenticate('wrong@email.com', 'strong_password')
       ).to be_nil
     end
   end
