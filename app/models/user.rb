39d3ed14e4b461913eaa782ace3965049a6ac2cd
Matthew Daniel Murphy <matt@mattmurphy.ca>
2018-01-22 20:37:17 -0500

    Add password authentication class
    add class to user model and call it in session controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true, length: { minimum: 10 }
   validates :email, uniqueness: { case_sensitive: false }
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    return user if user && user.authenticate(password)
+    nil
+  end
 end
